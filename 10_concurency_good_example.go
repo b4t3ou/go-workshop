eee85a4b6af2019f4371c8f3a2017a519d9c04e6
Kovács Zoltán <zoltan.smith@gmail.com>
2016-07-14 11:44:25 +0100

    Added concurency

---
+++ 10_concurency_good_example.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var items []int
+var writer chan int
+
+func main() {
+	writer = make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		for {
+			item := <-writer
+			items = append(items, item)
+		}
+	}()
+
+	go appendToItems(3, &wg, "one")
+	go appendToItems(2, &wg, "two")
+
+	wg.Wait()
+	fmt.Println(items)
+	fmt.Println("Done")
+}
+
+func appendToItems(multiplier int, mainWG *sync.WaitGroup, name string) {
+	wg := sync.WaitGroup{}
+	counter := random(1, 100)
+	wg.Add(counter - 1)
+
+	for i := 1; i < counter; i++ {
+		go func(counter int) {
+			duration := random(1, 100)
+			time.Sleep(time.Duration(random(1, 100)) * time.Millisecond)
+			writer <- duration
+			fmt.Println(name + " - " + strconv.Itoa(duration))
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	mainWG.Done()
+}
+
+func random(min, max int) int {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	return rand.Intn(max-min) + min
+}
